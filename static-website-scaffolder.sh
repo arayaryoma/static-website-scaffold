52d1fa466209e8ed8dfd4aff8b1a7d8186af0ec1
Allajah <r1o.ryoma.abe@gmail.com>
2018-02-13 13:31:42 +0900

    Fix directory structure

--- static-website-scaffolder.sh
+++ static-website-scaffolder.sh
@@ -39,19 +39,19 @@ mkdir -p public
 
 curl -L -s "https://www.gitignore.io/api/macos,linux,node" > .gitignore
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/index.pug" > src/templates/index.pug
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/templates/index.pug" > src/templates/index.pug
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/index.js" > src/scripts/index.js
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/scripts/index.js" > src/scripts/index.js
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/main.styl" > src/styles/main.styl
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/styles/main.styl" > src/styles/main.styl
 
 curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/webpack.config.js" > webpack.config.js
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/ja.yml" > src/assets/i18n/ja.yml
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/i18n/ja.yml" > src/assets/i18n/ja.yml
 
 curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/.editorconfig" > .editorconfig
 
-curl -o src/assets/images/michelle.gif "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/michelle.gif"
+curl -o src/assets/images/michelle.gif "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/images/michelle.gif"
 
 npm install add-npm-scripts
 ./node_modules/.bin/add-npm-scripts start "webpack-dev-server"
