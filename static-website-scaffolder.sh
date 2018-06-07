4edd3998d0dfaf1cd34903a4b7f1d8c0e412663b
arayaryoma <r1o.ryoma.abe@gmail.com>
2018-06-08 02:35:08 +0900

    Fix remote url

--- static-website-scaffolder.sh
+++ static-website-scaffolder.sh
@@ -49,21 +49,21 @@ mkdir -p public
 
 curl -L -s "https://www.gitignore.io/api/macos,linux,node" > .gitignore
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/templates/index.pug" > src/templates/index.pug
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/fix-dir-structure/src/templates/index.pug" > src/templates/index.pug
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/scripts/index.js" > src/scripts/index.js
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/fix-dir-structure/src/scripts/index.js" > src/scripts/index.js
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/styles/main.styl" > src/styles/main.styl
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/fix-dir-structure/src/styles/main.styl" > src/styles/main.styl
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/styles/fonts.styl" > src/styles/fonts.styl
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/fix-dir-structure/src/styles/fonts.styl" > src/styles/fonts.styl
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/webpack.config.js" > webpack.config.js
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/fix-dir-structure/webpack.config.js" > webpack.config.js
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/i18n/ja.yml" > src/assets/i18n/ja.yml
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/fix-dir-structure/src/assets/i18n/ja.yml" > src/assets/i18n/ja.yml
 
-curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/.editorconfig" > .editorconfig
+curl -L -s "https://raw.githubusercontent.com/Allajah/static-website-scaffold/fix-dir-structure/.editorconfig" > .editorconfig
 
-curl -o src/assets/images/michelle.gif "https://raw.githubusercontent.com/Allajah/static-website-scaffold/master/lib/images/michelle.gif"
+curl -o src/assets/images/michelle.gif "https://raw.githubusercontent.com/Allajah/static-website-scaffold/fix-dir-structure/src/assets/images/michelle.gif"
 
 git clone https://github.com/Allajah/AlphaNSCjp.git src/assets/fonts/AlphaNSCjp
 rm -rf src/assets/fonts/AlphaNSCjp/.git
